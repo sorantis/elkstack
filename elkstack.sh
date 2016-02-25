d2ea999df2039d1dc7c8deeddad09c1f959a4b12
Dimitri Mazmanov <dimitri.mazmanov@ericsson.com>
2016-02-25 15:31:56 +0100

    Add delay between service startup and template registration

--- elkstack.sh
+++ elkstack.sh
@@ -6,20 +6,30 @@ echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee
 sudo apt-get update
 sudo apt-get -y install unzip
 sudo apt-get -y install openjdk-7-jre
-sudo apt-get --force-yes -y install elasticsearch
-sudo apt-get --force-yes -y install logstash
-sudo apt-get --force-yes -y install kibana
 
-# Instal beats plugin for logstash
+sudo apt-get --force-yes -y install elasticsearch
+sudo service elasticsearch restart
+sudo update-rc.d elasticsearch defaults 95 10
+
+
+sudo apt-get --force-yes -y install logstash
+# Install beats plugin for logstash
 sudo /opt/logstash/bin/plugin install logstash-input-beats
 sudo /opt/logstash/bin/plugin update logstash-input-beats
 
 # Copy logstash configuration files
 sudo cp conf/logstash/* /etc/logstash/conf.d/
 
-sudo service elasticsearch start
-sudo service logstash start
+sudo service logstash restart
+sudo update-rc.d logstash defaults 96 9
+
+sudo apt-get --force-yes -y install kibana
 sudo service kibana start
+sudo update-rc.d kibana defaults 96 9
+
+while ! nc -q 1 localhost 9200 </dev/null; do sleep 3; done
+
+curl --retry 20 -XPUT 'http://localhost:9200/_template/filebeat?pretty' -d@conf/filebeat.template.json
 
 # Install beats dashboards
 curl -L -O https://download.elastic.co/beats/dashboards/beats-dashboards-1.1.1.zip
@@ -27,9 +37,3 @@ unzip -o beats-dashboards-1.1.1.zip > /dev/null 2>&1
 cd beats-dashboards-1.1.1/
 ./load.sh
 cd ..
-
-curl -XPUT 'http://localhost:9200/_template/filebeat?pretty' -d@conf/filebeat.template.json
-
-
-#sudo service logstash restart
-#sudo update-rc.d logstash defaults 96 9
